d380f3bd020124fa846d5f62cbb172996d1d5097
paramonovdan <paramonovdan08@gmail.com>
2026-09-11 15:02:12 +0300

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream> 
+#include <iostream>
 int main() {
-   std::cout << "Hello, I'm Даниил Парамонов from group ИУ5-13/4!" << std::endl;
-    return 0;
-}
+    std::cout << "Updated message!" << std::endl;
+     return 0;
+    }
