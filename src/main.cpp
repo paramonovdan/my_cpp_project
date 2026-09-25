d8182bf9a7b8072b7d86617d5ba810b146491385
paramonovdan <paramonovdan08@gmail.com>
2026-09-25 13:11:54 +0300

    Final update and cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Updated message!" << std::endl;
+    cout << "Hello, I'm Daniil Paramonov from group ИУ5-13/4!" << std::endl;
      return 0;
     }
